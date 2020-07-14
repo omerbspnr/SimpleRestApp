fd2e17237b9b3b9053b66bba4b408a18bc95b09a
omerbspnr <baspinaromer1996@gmail.com>
2020-07-14 11:21:49 +0300

    test for password hashing

--- src/main/java/org/csystem/app/service/UserService.java
+++ src/main/java/org/csystem/app/service/UserService.java
@@ -2,6 +2,8 @@ package org.csystem.app.service;
 
 import org.csystem.app.entity.User;
 import org.csystem.app.repository.IUserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,19 +13,26 @@ import java.util.stream.StreamSupport;
 public class UserService implements IUserService {
     private IUserRepository m_userRepository;
 
-    public UserService(IUserRepository userRepository)
+    private PasswordEncoder m_encoder;
+
+    public UserService(IUserRepository userRepository, PasswordEncoder encoder)
     {
         m_userRepository = userRepository;
+        m_encoder = encoder;
     }
 
     @Override
     public User save(User u)
     {
+        u.setPassword(m_encoder.encode(u.getPassword()));
+        System.out.printf("Kayıt sifresi : %s%n", u.getPassword());
         return m_userRepository.save(u);
     }
     @Override
     public Optional<User> controlForLogin(User u)
     {
+        u.setPassword(m_encoder.encode(u.getPassword()));
+        System.out.printf("Giris sifresi : %s%n",u.getPassword());
         return StreamSupport
                 .stream(m_userRepository.findAll().spliterator(),false)
                 .filter(i -> i.getPassword().equals(u.getPassword()) && i.getUsername().equals(u.getUsername()))
